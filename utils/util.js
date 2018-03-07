629bb213349ebd57fc52f5bf4df35acef92066dc
worong <hello@woicon.com>
2018-03-07 16:23:13 +0800

    fixed 时间兼容问题

--- utils/util.js
+++ utils/util.js
@@ -1,15 +1,14 @@
 function formatTime(time) {
 
-    var time = time.replace(/-/g, ':').replace(' ', ':')
-    var time = time.split(':')
-    var date = new Date(time[0], (time[1] - 1), time[2], time[3], time[4], time[5])
-
-    var year = date.getFullYear()
-    var month = date.getMonth() + 1
-    var day = date.getDate()
-    var hour = date.getHours()
-    var minute = date.getMinutes()
-    var second = date.getSeconds()
+    var time = time.replace(/-/g, ':').replace(' ', ':'),
+        time = time.split(':'),
+        date = new Date(time[0], (time[1] - 1), time[2], time[3], time[4], time[5]),
+        year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate(),
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        second = date.getSeconds()
     //return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
     return [year, month, day].map(formatNumber).join('-')
 }
